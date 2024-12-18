14c8284f29f548b09ceb16f70f7b9d0a8d621c3d
lleomllr <88625636+lleomllr@users.noreply.github.com>
2024-12-18 16:58:55 +0100

    Update App.java

--- Chang_Roberts_Akka/Akka/src/main/java/sd/akka/App.java
+++ Chang_Roberts_Akka/Akka/src/main/java/sd/akka/App.java
@@ -16,8 +16,52 @@ public class App {
         ActorSystem actorSystem = ActorSystem.create(); 
 
         int numActor = 12; 
+	int cpt = 1; 
+        List<Integer> IDList = generateIDList(numActors);
 
-        int[] alrChoose = new int[numActor]; 
+        // Création des acteurs avec des ID uniques
+        ActorRef[] actors = new ActorRef[numActors];
+        for (int i = 0; i < numActors; i++) {
+            int ID = IDList.get(i);
+            actors[i] = actorSystem.actorOf(CR_actor.props(ID), "" + cpt);
+            System.out.println("Actor n°" + cpt + " ID : " + ID);
+            cpt++;
+        }
+
+        // Initialisation de l'anneau
+        for (int i = 0; i < numActors; i++) {
+            ActorRef currentActor = actors[i];
+            ActorRef neighbor = actors[(i + 1) % numActors];
+            currentActor.tell(new CR_actor.CreateRing(neighbor), ActorRef.noSender());
+        }
+
+        // Choix d'un acteur aléatoire pour lancer l'élection
+        int firstIndex = ThreadLocalRandom.current().nextInt(numActors);
+        actors[firstIndex].tell(new CR_actor.StartElection(), ActorRef.noSender());
+
+        // Attente pour permettre à l'élection de se terminer
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Arrêt du système d'acteurs
+        actorSystem.terminate();
+    }
+
+    public static List<Integer> generateIDList(int numActors) {
+        Set<Integer> generatedNumbers = new HashSet<>();
+        Random random = new Random();
+
+        while (generatedNumbers.size() < numActors) {
+            int randomInt = random.nextInt(numActors * 10);
+            generatedNumbers.add(randomInt);
+        }
+
+        return new ArrayList<>(generatedNumbers);
+    }
+        /*int[] alrChoose = new int[numActor]; 
         ActorRef[] actor = new ActorRef[numActor]; 
 
         for(int i = 0; i < numActor; i++){
@@ -70,5 +114,5 @@ public class App {
         }
 
         actorSystem.terminate(); 
-    }
+    }*/
 }
